17d6880d481915aec41519362813d80ab7585389
delpierro4ever <delpierro4ever@gmail.com>
2023-11-15 14:38:53 -0800

    commiting main dashboard

--- app/ui/dashboard/rightbar/Rightbar.jsx
+++ app/ui/dashboard/rightbar/Rightbar.jsx
@@ -1,8 +1,26 @@
+import Image from 'next/image'
 import styles from './rightbar.module.css'
+import { MdPlayCircleFilled } from 'react-icons/md'
 
 const Rightbar = () => {
   return (
-    <div className={styles.container}>Rightbar</div>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.bgContainer}>
+          <Image src="/astronaut.png"  alt='' fill/>
+        </div>
+        <div className={styles.text}>
+          <span className={styles.notification}>Available Now</span>
+          <h3>How to win the new version of the admin dashboard </h3>
+          <span className={styles.subtitle}>Takes 4 minutes to learn</span>
+          <p className={styles.description}>Lorem ipsum color sit emet connector dicipline ilit remember eius liberes perspecitvie recursive position</p>
+          <button className={styles.button}><MdPlayCircleFilled /> Learn</button>
+        </div>
+      </div>
+      {/* <div className={styles.bgContainer}>
+          <Image src="/astronaut.png"  alt='' fill/>
+        </div> */}
+    </div>
   )
 }
 
